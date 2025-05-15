0b212edf9d2cc860e9b6d10f74bb7005fadb86c4
wangy <wangy325@qq.com>
2025-05-16 01:19:53 +0800

    设置界面的样式2

---
+++ src/helper/TabItemHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Irvue_win.src.helper
+{
+    public static class TabItemHelper
+    {
+        public static readonly DependencyProperty IconSourceProperty =
+            DependencyProperty.RegisterAttached(
+                "IconSource",
+                typeof(ImageSource),
+                typeof(TabItemHelper),
+                new FrameworkPropertyMetadata(null)
+            );
+
+        public static ImageSource GetIconSource(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(IconSourceProperty);
+        }
+
+        public static void SetIconSource(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(IconSourceProperty, value);
+        }
+    }
+
+}
